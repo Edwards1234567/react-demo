4ea27eaad2d2d7d228dc1f167a841b3b90ebad5c
Joseph jiang <hedijiang2018@gmail.com>
2018-09-30 11:53:45 +1000

    Add dashboard and login component add Router in project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Login from './component/Login';
+import Dashboard from './component/Dashboard';
+import {
+  Switch,
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link
+} from 'react-router-dom';
+const App=()=>{
+  return(
+    <Router>
+      <Switch>
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Redirect path="/" to="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/component/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Dashboard extends React.Component{
+
+    constructor(){
+            super();
+
+    }
+
+    render(){
+        return(
+            <div>
+                Dashboard!!!
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
